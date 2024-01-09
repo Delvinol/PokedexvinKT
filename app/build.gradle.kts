96fc669b8a5f9897b1200a4becc19992f838c948
DelvinTec <delvinonlm@gmail.com>
2024-01-08 20:28:48 -0500

    creation of first view and adding a dependecy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
+    // Para el switch
+    implementation("com.tbuonomo:dotsindicator:5.0")
     // Para el retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
